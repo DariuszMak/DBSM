31752d9b5492d9fd6896d061965242f49456314a
DariuszMak <darekmakarewicz1994@gmail.com>
2017-10-08 17:09:21 +0200

    second activity

--- TestApp/app/src/main/java/com/example/dariusz/testapp/MainActivity.java
+++ TestApp/app/src/main/java/com/example/dariusz/testapp/MainActivity.java
@@ -2,11 +2,13 @@ package com.example.dariusz.testapp;
 
 import android.app.ActionBar;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 
@@ -46,5 +48,11 @@ public class MainActivity extends AppCompatActivity {
 
 
         editor.commit();
+    }
+
+    public void chceckPassword (View view)
+    {
+        Intent intent = new Intent(this, MessageActivity.class);
+        startActivity(intent);
     }
 }
